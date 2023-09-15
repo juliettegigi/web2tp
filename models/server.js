2d36e14db3930c44fdfd41f1fe582d2ef515b245
Marina Julieta Gutierrez <111161094+juliettegigi@users.noreply.github.com>
2023-09-15 07:10:05 -0300

    Actualizar server.js

--- models/server.js
+++ models/server.js
@@ -34,7 +34,7 @@ export default class Server{
 
     middlewares(){
         this.app.use(cors(this.corsOptions));
-      app.use((req, res, next) => {
+      this.app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://web2tp.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
